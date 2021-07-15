30b5104dfdabddde94aa95dbc2c34a28ae430a5a
MugeraH <bwosihughes24@gmail.com>
2021-07-15 23:02:32 +0300

    show all users

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./Home.scss";
 
 import Loader from "../loader/Loader";
+import FilterUsers from "../FIlterUsers/FIlterUsers";
 
 import { useSelector, useDispatch } from "react-redux";
 import { loadUsers } from "../../actions/usersAction";
@@ -18,19 +19,26 @@ function Home() {
     <div className="container">
       <div className="title">
         <h2>Users</h2>
+        <FilterUsers />
       </div>
-
-      {users ? (
-        users.map((user, index) => {
-          return (
-            <div key={index}>
-              <div className="user-container">{user.name}</div>
-            </div>
-          );
-        })
-      ) : (
-        <Loader />
-      )}
+      <div className="user-container">
+        {users ? (
+          users.map((user, index) => {
+            return (
+              <div className="user-wrapper" key={index}>
+                <div>
+                  <h4>{user.name}</h4>
+                  <h5 className="role">{user.occupation}</h5>
+                  <p>{user.bio}</p>
+                  <h5>{user.email}</h5>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <Loader />
+        )}
+      </div>
     </div>
   );
 }
