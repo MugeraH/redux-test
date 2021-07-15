497771c0334894816299bdd25afb3dd9619ab37a
MugeraH <bwosihughes24@gmail.com>
2021-07-15 18:32:54 +0300

    setting up redux config

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk)) //comibine dev tools and thunk in one line
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
