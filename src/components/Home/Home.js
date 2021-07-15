bbb1451e6ac40d79724a50bc79876f61c827fcb0
MugeraH <bwosihughes24@gmail.com>
2021-07-15 22:13:43 +0300

    load users to home view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from "react";
 import "./Home.scss";
 
+import { useSelector, useDispatch } from "react-redux";
+import { loadUsers } from "../../actions/usersAction";
+
 function Home() {
+  const { users } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUsers());
+    console.log(users.length);
+  }, [dispatch]);
   return <div className="container"></div>;
 }
 
